88213f91a3a64b2339799858ac7b2aeb3fb8ecfd
koulouch <redouane@creative-technology.io>
2025-06-03 00:14:01 +0200

    mock MailService in SpringBootSecurityJwtApplicationTests to improve test isolation

--- src/test/java/com/cdg/springjwt/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/com/cdg/springjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,13 +1,17 @@
 package com.cdg.springjwt;
 
+import com.cdg.springjwt.services.MailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @MockBean
+    private MailService mailService;
 
+    @Test
+    public void contextLoads() {
+    }
 }
